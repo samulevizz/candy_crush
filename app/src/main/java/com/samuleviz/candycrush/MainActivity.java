1d02eeb473738a3f45bbe838a7af74d90b750e5f
Samu-dev8 <levizzarisamuele@gmail.com>
2024-04-17 19:05:14 +0200

    ciao

--- app/src/main/java/com/samuleviz/candycrush/MainActivity.java
+++ app/src/main/java/com/samuleviz/candycrush/MainActivity.java
@@ -1,24 +1,90 @@
 package com.samuleviz.candycrush;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    int[] candies = {
+            R.drawable.blu,
+            R.drawable.rossa,
+            R.drawable.gialla,
+            R.drawable.verde,
+            R.drawable.liquirizia,
+            R.drawable.cioccolata,
+            R.drawable.viola,
+            R.drawable.arancia
+    };
+    int widthOfBlock, noOfBlocks = 8, widthOfScreen;
+    ArrayList<ImageView> candyImages = new ArrayList<>();
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        widthOfScreen = displayMetrics.widthPixels;
+        int heightOfScreen = displayMetrics.heightPixels;
+        widthOfBlock = widthOfScreen / noOfBlocks;
+        createBoard();
+        for (ImageView imageView : candyImages) {
+            imageView.setOnTouchListener(new OnSwipeListener(this) {
+                @Override
+                void SwipeLeft() {
+                    super.SwipeLeft();
+                    Toast.makeText(MainActivity.this, "Left", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                void SwipeRight() {
+                    super.SwipeRight();
+                    Toast.makeText(MainActivity.this, "Right", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                void SwipeUp() {
+                    super.SwipeUp();
+                    Toast.makeText(MainActivity.this, "Up", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                void SwipeDown() {
+                    super.SwipeDown();
+                    Toast.makeText(MainActivity.this, "Down", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private void createBoard() {
+        GridLayout gridLayout = findViewById(R.id.board);
+        gridLayout.setRowCount(noOfBlocks);
+        gridLayout.setColumnCount(noOfBlocks);
+        gridLayout.getLayoutParams().width = widthOfScreen;
+        gridLayout.getLayoutParams().height = widthOfScreen;
+        for (int i = 0; i < noOfBlocks * noOfBlocks; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setId(i);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(widthOfBlock, widthOfBlock));
+            imageView.setMaxHeight(widthOfBlock);
+            imageView.setMaxWidth(widthOfBlock);
+            int randomCandyIndex = (int) Math.floor(Math.random() * candies.length);
+            imageView.setImageResource(candies[randomCandyIndex]);
+            candyImages.add(imageView);
+            gridLayout.addView(imageView);
+        }
     }
 }
---
+++ app/src/main/java/com/samuleviz/candycrush/OnSwipeListener.java
@@ -0,0 +1,75 @@
+package com.samuleviz.candycrush;
+
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class OnSwipeListener implements View.OnTouchListener {
+    private final GestureDetector gestureDetector;
+
+    public OnSwipeListener(Context context) {
+        gestureDetector = new GestureDetector(context, new GestureListener());
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    }
+
+    private final class GestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final int SWIPE_THRESHOLD = 100;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            boolean result = false;
+            float diffY = e2.getY() - e1.getY();
+            float diffX = e2.getX() - e1.getX();
+            if (Math.abs(diffX) > Math.abs(diffY)) {
+                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffX > 0) {
+                        SwipeRight();
+                    } else {
+                        SwipeLeft();
+                    }
+                    result = true;
+                }
+            } else {
+                if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        SwipeDown();
+                    } else {
+                        SwipeUp();
+                    }
+                    result = true;
+                }
+            }
+            return result;
+        }
+    }
+
+    void SwipeLeft() {
+        // Implement SwipeLeft action
+    }
+
+    void SwipeRight() {
+        // Implement SwipeRight action
+    }
+
+    void SwipeUp() {
+        // Implement SwipeUp action
+    }
+
+    void SwipeDown() {
+        // Implement SwipeDown action
+    }
+}
